0455f6ac532a018ddbfbc8c458e8a990988d43fe
ValeriyLapin <valeriylapin@gmail.com>
2023-12-05 05:19:05 +0100

    Clean up main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'roulette/roulette.dart';
-
-const methodChannel = MethodChannel('com.amco.cs');
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
